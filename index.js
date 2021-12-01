8f6acdaa3b1f585a7306339c765e326a68e539e1
Austin <austin.j.fitzhugh@gmail.com>
2021-11-30 19:19:20 -0500

    commit tree 932fed806d64d98342c11e16e962f2ae85b41bb6

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+
+const formData = {
+    name: "Steve",
+    email: "steve@steve.com",
+  };
+  
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+  
+function submitData(){
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      return console.log(object);
+    })
+    .catch(function (error) {
+        alert("Unauthorized Access");
+        console.log(error.message);
+      });
+  }
+
+  submitData();
